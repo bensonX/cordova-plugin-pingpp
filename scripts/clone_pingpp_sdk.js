cc789a5e39854ad84133aa9f3792d0ea696d5391
caohongtao <cht951@gmail.com>
2016-04-22 23:19:48 +0800

    Update clone_pingpp_sdk.js

--- scripts/clone_pingpp_sdk.js
+++ scripts/clone_pingpp_sdk.js
@@ -10,15 +10,16 @@ module.exports = function(ctx) {
     , style = r('ansi-styles')
     , platform = ctx.opts.plugin.platform
     , sdk_path = path.join(ctx.opts.plugin.dir, 'sdk', platform)
-    , sdk_url = 'https://github.com/PingPlusPlus/pingpp-'+platform+'.git --depth=1';
+    , sdk_url = 'https://github.com/PingPlusPlus/pingpp-'+platform+'.git';
 
   if (shell.test('-e', sdk_path)) { d.resolve(); }
   else {
     if (!shell.which('git')) d.reject('Can not request git');
 
     shell.echo(style.cyan.open + 'Cloning ping++ ' + platform + ' SDK, Please wait...' + style.cyan.close);
-
-    shell.exec(['git', 'clone', sdk_url, sdk_path].join(' '), {stdio: 'inherit'}).code == 0
+    shell.echo(['git', 'clone', sdk_url, sdk_path, '--depth=1'].join(' ') + style.cyan.close);
+    
+    shell.exec(['git', 'clone', sdk_url, sdk_path, '--depth=1'].join(' '), {stdio: 'inherit'}).code == 0
       ? d.resolve()
       : d.reject('Clone ' + platform + ' SDK failed!');
   }
